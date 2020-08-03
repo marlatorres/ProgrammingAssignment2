d8e232ead5e37d1a025715f59ba1a5e80ac78476
marlatorres <69070149+marlatorres@users.noreply.github.com>
2020-08-03 10:02:02 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions are used to solve for the inverses of invertible matrices. 
 
-## Write a short comment describing this function
+## This function sets its inverse as null at first and caches it once set. 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        mt <- NULL
+        set <- function(y) {
+                m <<- y     #Caches m
+                mt <<- null   #Caches mt
+        }
+        get <- function() m     #gets the original matrix m
+        setI <- function(inverse) mt <<- inverse    #function to set the inverse mt
+        getI <- function() mt     #returns the inverse matrix
+        list(set = set, get = get, setI = setI, getI = getI)
 }
 
+## This function tries to check if there is a cached inverse to the set matrix. If
+## there is none yet, it goes back by getting the original matrix and then
+## proceeds to getting its inverse
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        mt <- m$getI()          
+        if(!is.null(mt)) {     #Checks if there is an inverse already
+                message("Getting cached data...")
+                return(mt)
+        }
+        data <- m$get()      #Gets the matrix m
+        mt <- solve(data,...)   # Solves for the inverse of m
+        m$setI(mt)     #Sets the inverse to mt
+        mt             # Returns mt
 }
+
+##End
